e7981df658f80b1623b27426478ea8aa6f8dc2ca
Justine <justynakomarowska@hotmail.com>
2016-06-03 00:30:25 +0200

    Dodanie randomowych itemów i pustych slotów

--- core/src/com/game/Inventory/Inventory.java
+++ core/src/com/game/Inventory/Inventory.java
@@ -17,16 +17,16 @@ public class Inventory {
 			slots.add(new Slot(null, 0));
 		}
 
-//		// create some random items
-//		for (Slot slot : slots) {
-//			slot.add(Item.values()[MathUtils.random(0, Item.values().length - 1)], 1);
-//		}
-//
-//		// create a few random empty slots
-//		for (int i = 0; i < 3; i++) {
-//			Slot randomSlot = slots.get(MathUtils.random(0, slots.size - 1));
-//			randomSlot.take(randomSlot.getAmount());
-//		}
+		// create some random items
+		for (Slot slot : slots) {
+			slot.add(Item.values()[MathUtils.random(0, Item.values().length - 1)], 1);
+		}
+
+		// create a few random empty slots
+		for (int i = 0; i < 3; i++) {
+			Slot randomSlot = slots.get(MathUtils.random(0, slots.size - 1));
+			randomSlot.take(randomSlot.getAmount());
+		}
 	}
 
 	public int checkInventory(Item item) {
@@ -41,35 +41,35 @@ public class Inventory {
 		return amount;
 	}
 
-	public boolean store(Item item, int amount) {
-		Slot itemSlot = firstSlotWithItem(item);
-		if (itemSlot != null) {
-			itemSlot.add(item, amount);
-			return true;
-		} else {
-			Slot emptySlot = firstSlotWithItem(null);
-			if (emptySlot != null) {
-				emptySlot.add(item, amount);
-				return true;
-			}
-		}
+	// public boolean store(Item item, int amount) {
+	// 	Slot itemSlot = firstSlotWithItem(item);
+	// 	if (itemSlot != null) {
+	// 		itemSlot.add(item, amount);
+	// 		return true;
+	// 	} else {
+	// 		Slot emptySlot = firstSlotWithItem(null);
+	// 		if (emptySlot != null) {
+	// 			emptySlot.add(item, amount);
+	// 			return true;
+	// 		}
+	// 	}
 
-		// no slot to add
-		return false;
-	}
+	// 	// no slot to add
+	// 	return false;
+	// }
 
 	public Array<Slot> getSlots() {
 		return slots;
 	}
 
-	private Slot firstSlotWithItem(Item item) {
-		for (Slot slot : slots) {
-			if (slot.getItem() == item) {
-				return slot;
-			}
-		}
+	// private Slot firstSlotWithItem(Item item) {
+	// 	for (Slot slot : slots) {
+	// 		if (slot.getItem() == item) {
+	// 			return slot;
+	// 		}
+	// 	}
 
-		return null;
-	}
+	// 	return null;
+	// }
 
 }
